d4e518179fc257a2bcdb07750cbbf4200f181153
DENIS PLASTER <denjeo@gmail.com>
2018-11-18 14:49:08 -0600

    files setup

---
+++ client/src/components/Header.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+        <nav>
+            <div className="nav-wrapper">
+            <a className="left brand-logo">
+            FunFund
+            </a>
+            <ul className="right">
+            <li>
+                <a>Login with Google</a>
+                </li>
+                </ul>
+                </div>
+                </nav>
+    );
+  }
+}
+
+export default Header;
---
+++ client/src/index.js
@@ -0,0 +1,18 @@
+import 'materialize-css/dist/css/materialize.min.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+
+import App from "./components/App";
+import reducers from "./reducers";
+
+const store = createStore(reducers, {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+
+  document.querySelector("#root")
+);
