f659e65f786988f1534e4fe4f40da9d0cc0d9053
DENIS PLASTER <denjeo@gmail.com>
2018-11-19 12:32:02 -0600

    files setup 2.1

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,20 +2,21 @@ const passport = require("passport");
 
 module.exports = app => {
   app.get(
-    "./auth/google",
+    "/auth/google",
     passport.authenticate("google", {
       scope: ["profile", "email"]
     })
   );
 
-  // app.get('./auth/google/callback', passport.authenticate{''})
 
-  app.get("./api/logout", (requestAnimationFrame, res) => {
+app.get("/auth/google/callback", passport.authenticate("google"));
+
+  app.get("/api/logout", (req, res) => {
     req.logout();
     res.send(req.user);
   });
 
-  app.get("./api/current_user", (req, res) => {
+  app.get("/api/current_user", (req, res) => {
     res.send(req.user);
   });
 };
