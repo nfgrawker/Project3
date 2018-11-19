e042c9a048c6fc4c30fb6ba2a30d955e2661cd4b
DENIS PLASTER <denjeo@gmail.com>
2018-11-18 19:12:03 -0600

    files setup 1.1

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+app.get('/', (req, res) => {
+  res.send({ fun: 'fund' });
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(5000);
+app.listen(PORT);
