183a1d003db5ee0f6a46764c880c3842b0dbfe03
achrysler <achrysler36@gmail.com>
2018-11-20 16:29:30 -0600

    added a donor seeder

--- Models/Raffle.js
+++ Models/Raffle.js
@@ -5,6 +5,7 @@ const raffleSchema = new Schema({
   title: { type: String, required: true },
   prize: { type: String, required: true },
   descriptions: { type: String},
+  sponsor: {type: String},
   endDate: { type: String, required: true},
   nonProfit: [{type:Schema.Types.ObjectId, ref:"nonProfit"}],
   donations: [{type:Schema.Types.ObjectId, ref:"Donation"}],
