4e47e6a45296a5c2dbad316e2936e61bcf2aed60
nfgrawker <nfgrawker@gmail.com>
2018-11-25 16:52:16 -0600

    changes to routes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,28 +1,41 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import axios from "axios";
 require('./App.css')
 
 class Header extends Component {
-  renderContent() {
-    switch (this.props.auth) {
-      case null:
-        return;
-      case false:
-        return (
-          <li>
-            <a href="http://localhost:3001/auth/google">Login</a>
-          </li>
-        );
-      default:
-        return (
-          <li>
-            <a href="/api/logout">Logout</a>
-          </li>
-          //  <li>
-          //      <img src={user.thumnail}/>
-          // </li>
-        );
+    state={
+        auth:null
+    }
+
+    getUser = ()=>{
+        axios.get("http://localhost:3001/api/currentuser").then(response=>{
+            console.log("yep")
+            console.log(response)
+        })
+    }
+
+  renderContent = ()=> {
+      this.getUser()
+      switch (this.state.auth) {
+          case null:
+            return;
+          case false:
+            return (
+              <li>
+                <a href="http://localhost:3001/auth/google">Login</a>
+              </li>
+            );
+          default:
+            return (
+              <li>
+                <a href="/api/logout">Logout</a>
+              </li>
+              //  <li>
+              //      <img src={user.thumnail}/>
+              // </li>
+            );
     }
   }
 
@@ -43,11 +56,9 @@ class Header extends Component {
     );
   }
   }
-  function mapStateToProps({auth}) {
+
+function mapStateToProps({auth}) {
     return{auth};
-}
-function mapStateToProps({ auth }) {
-  return { auth };
 }
 
 export default connect(mapStateToProps)(Header);
