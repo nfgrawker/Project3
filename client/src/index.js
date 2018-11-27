9b53828c3fbbc9ab1c3ee57f6333b21bfc6d331f
DENIS PLASTER <denjeo@gmail.com>
2018-11-27 13:00:11 -0600

    added default profile image

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import reduxThunk from 'redux-thunk';
-import App from "./components/App";
+import App from "./App";
 import reducers from "./reducers";
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
