51a6a9d915595fbbce76e5fa43991a3af4cb0bd1
nfgrawker <nfgrawker@gmail.com>
2018-11-28 11:27:36 -0600

    new models folder

---
+++ models/Donation.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const donationSchema = new Schema({
+  name: { type: String, required: true },
+  date: { type: Date, default: Date.now},
+  donations: [{type:Schema.Types.ObjectId, ref:"Donations"}]
+  
+});
+
+const Donation = mongoose.model("Donation", donationSchema);
+
+module.exports = Donation;
