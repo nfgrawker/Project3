9b44468a4e21342bbe6e7d0f0855590be418bba0
DENIS PLASTER <denjeo@gmail.com>
2018-11-29 12:53:15 -0600

    prize page layout

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Home from './components/pages/Landing'
 import Profile from './components/pages/Profile'
-import Checkout from './components/pages/Checkout/Index'
+import Checkout from './components/pages/Checkout/index'
+import Prizes from './components/pages/Prizes/index'
 
 
 
@@ -17,6 +18,7 @@ const App = () => {
           <Route exact path="/" component={Home} />
           <Route exact path="/profile" component={Profile} />
           <Route exact path="/checkout" component={Checkout} />
+          <Route exact path="/prizes" component={Prizes} />
 
         </div>
       </BrowserRouter>
