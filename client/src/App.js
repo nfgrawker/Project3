5200fee2c73613be0e7c18d10f73a288dd329872
nfgrawker <nfgrawker@gmail.com>
2018-11-29 14:34:41 -0600

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Landing from './components/pages/Landing'
-import Profile from './components/pages/Profile'
+import Profile from './components/pages/Profile.js'
 import Checkout from './components/pages/Checkout/Index'
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -11,10 +11,10 @@ import AdminPage from './components/pages/AdminPage'
 const App = () => {
   return (
     <div>
-        <Header />
         <BrowserRouter>
             <div>
-                <Route exact path="/" component={Home} />
+                <Route path="/" component={Header} />
+                <Route exact path="/" component={Landing} />
                 <Route exact path="/profile" component={Profile} />
                 <Route exact path="/checkout" component={Checkout} />
                 <Route exact path='/admin'  component={AdminPage} />
