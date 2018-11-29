a0fda3d689965d202669cdc3b5b56d79f58b0006
kkasperek <kt.kasperek@gmail.com>
2018-11-29 10:46:47 -0600

    updated file names

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import Home from './components/pages/Landing'
+import Landing from './components/pages/Landing'
 import Profile from './components/pages/Profile'
 import Checkout from './components/pages/Checkout/Index'
-import AdminPage from './components/pages/admin-page'
+import AdminPage from './components/pages/AdminPage'
 
 
 
@@ -14,7 +14,7 @@ const App = () => {
       <BrowserRouter>
         <div>
 
-          <Route exact path="/" component={Home} />
+          <Route exact path="/" component={Landing} />
           <Route exact path="/profile" component={Profile} />
           <Route exact path="/checkout" component={Checkout} />
           <Route exact path="/admin" component={AdminPage} />
