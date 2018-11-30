c4a029e2cfcad898085cf80b9d57b481e5e1d6d9
DENIS PLASTER <denjeo@gmail.com>
2018-11-30 11:40:43 -0600

    stop ignoring config keys

---
+++ config/keys.js
@@ -0,0 +1,13 @@
+module.exports = {
+    google: {
+     clientID:
+      "896288553963-700upfnriram341r3nleepvurue2hovi.apps.googleusercontent.com",
+     clientSecret: "xgUc1KswUbN5X1_0pQ4MFFH_"
+    },
+    mongodb: {
+     dbURI: "mongodb://thefirm:project3@ds247178.mlab.com:47178/project3"
+    },
+    session: {
+     cookieKey: "tjxwojbypincknevibowlknohrcinrepnwjcrwcrpinrc"
+    }
+   };
