1a6ade322120eee52ef15ad4991a1bb21525d62a
Iramsey0418 <saiah.ramsey18@gmail.com>
2018-12-02 21:36:16 -0600

    Tryin to still fix the fixes.

--- client/src/App.js
+++ client/src/App.js
@@ -9,11 +9,6 @@ import AdminPage from './components/pages/Admin/Admin';
 import PrizePage from './components/pages/Prizes/Prizes'
 import RafflePage from './components/pages/Raffle';
 import AppPay from "./components/pages/Payment/AppPay";
-<<<<<<< HEAD
-import Home from './components/pages/landing/LandingPage.jsx';
-=======
-
->>>>>>> 6d5ae4942b247f5c5f9d894b946e7b15d1d0d81c
 
 
 
