fffe8df158980ff7c716e5da42fcb8acacd0ca9b
Hertz <43124833+hertz043@users.noreply.github.com>
2018-12-03 16:16:17 -0600

    Raffle page styling

--- client/src/components/pages/Raffle/Raffle.js
+++ client/src/components/pages/Raffle/Raffle.js
@@ -1,26 +1,49 @@
 import React, { Component } from "react";
-import CardMedia from '@material-ui/core/Card'
-import Grid from '@material-ui/core/Grid';
-import NonProfitInfo from "../../NonProfitInfo";
 import classNames from "classnames";
 import withStyles from "@material-ui/core/styles/withStyles";
 
-import rafflePageStyle from "./style/raffleStyle"
+import Grid from '@material-ui/core/Grid';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+import rafflePageStyle from "./style/raffleStyle";
+require("./style/raffleStyle.css");
 
 class Raffle extends Component {
 
 render() {
   const { classes, ...rest } = this.props;
+
   return (
-    <div className={classNames(classes.main, classes.mainRaised)}>
-      <div className={classes.container}>
-        <h3>Signed Minnesota Vikings Helmet</h3>
-        <img
+    <div className="raffle-page">
+      <div className={classNames(classes.main, classes.mainRaised)}>
+        <div className={classes.container}>
+      <Grid>
+      <GridList className={classes.gridList} cols={1}>
+      <GridListTile key={classes.img} id="test">
+        <img 
+          className="prizeImage"
+          component="img"
           alt="Prize Image"
-          height="500"
           src="https://cdn8.bigcommerce.com/s-4erg8hlk42/images/stencil/1280x1280/products/51500/52423/Winfield-Vik__04752.1521656868.jpg?c=2"
           title="Vikings Helmet"
         />
+        </GridListTile>
+        </GridList>
+        </Grid>
+
+
+        <Paper className={classes.root} elevation={1}>
+          <Typography variant="h4" component="h4">
+            This is a sheet of paper.
+          </Typography>
+          <Typography component="p">
+            Paper can be used to build surface or other elements for your application.
+          </Typography>
+        </Paper>
+        </div>
       </div>
     </div>
   );
--- client/src/components/pages/Raffle/style/raffleStyle.js
+++ client/src/components/pages/Raffle/style/raffleStyle.js
@@ -3,7 +3,7 @@ import { container, title } from "./material-kit-react.jsx";
 const rafflePageStyle = {
   container: {
     zIndex: "12",
-    color: "#000000",
+    color: "rgba(0,0,0,0.87)",
     ...container
   },
   title: {
@@ -27,11 +27,20 @@ const rafflePageStyle = {
     zIndex: "3"
   },
   mainRaised: {
-    margin: "-60px 30px 0px",
+    margin: "20px 30px 0px",
     borderRadius: "6px",
     boxShadow:
       "0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)"
+  },
+  li: {
+    height: "0px !important",
+    boxShadow: "0px 1px 3px 0px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12)",
+    borderRadius: "4px"
+  },
+  h4: {
+    padding: [5, 5, 5, 5]
   }
+
 };
 
 export default rafflePageStyle;
