8e9ca5d8eea06418c5b5e34ef6b3fbb6609469e7
nfgrawker <nfgrawker@gmail.com>
2018-12-03 15:12:38 -0600

    got prize working

--- routes/nonprofitRoutes.js
+++ routes/nonprofitRoutes.js
@@ -6,14 +6,14 @@ module.exports = function(app) {
     // Load index page
     app.get("/api/nonprofit/:id", function (req, res) {
 
-        db.NonProfit.findById(req.params.id, function (err, result) {
+        NonProfit.findById(req.params.id, function (err, result) {
             if (err) console.log(err)
             res.send(result).end()
         })
     });
 
     app.post("/api/create/nonprofit", function (req, res) {
-        NonProfit.create({ image: req.body.image, name: req.body.name, address: req.body.address,
+        NonProfit.create({ imageLink: req.body.image, name: req.body.name, address: req.body.address,
             description:req.body.description, contactName: req.body.contactName, contactNumber: req.body.contactNumber, website: req.body.website }, function (err, small) {
             if (err) console.log(err);
             // saved!
