17dd2834e34b13e78244c5b838b02828fe565b67
nfgrawker <nfgrawker@gmail.com>
2018-12-04 14:12:54 -0600

    added create raffle route

--- routes/raffleRoutes.js
+++ routes/raffleRoutes.js
@@ -1,17 +1,27 @@
 const path = require("path");
-const db = require("../models")
-
+const mongoose = require('mongoose');
+const Raffle = mongoose.model("Prize");
 
 module.exports = function(app) {
     // Load index page
     app.get("/api/raffle/:id", function (req, res) {
-        db.Raffle.findById(id, function (err, result) {
+        Raffle.findById(id, function (err, result) {
             console.log(result)
         })
     });
 
     app.post("/api/create/raffle", function (req, res) {
-        res.end()
+        Raffle.create({ prize: req.body.prize,
+            nonProfit: req.body.nonProfit,
+            startTime: Date,
+            endTime: Date
+        }, function (err, result) {
+            if (err) console.log(err);
+            else{
+                console.log(result)
+            }
+        });
+        res.end();
     });
 
 };
