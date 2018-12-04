cb980f138852b6150d45e133b41607c5907b2f5a
achrysler <achrysler36@gmail.com>
2018-12-04 12:09:17 -0600

    prize card pulling data

--- client/src/components/RaffleItems/RaffleItems.js
+++ client/src/components/RaffleItems/RaffleItems.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
@@ -11,10 +11,13 @@ import './style.css';
 
 function shutter(i, els) {
   const wrapIndex = i => i % els.length
-   return els[wrapIndex(i)], els[wrapIndex(i + 1)], els[wrapIndex(i +2)]
- }
+   return  [els[wrapIndex(i)], els[wrapIndex(i + 1)], els[wrapIndex(i +2)] ]
+      }
 
-function RaffleItems(props) {
+ class RaffleItems extends Component {
+   state = {index:0}
+   render() {
+    console.log(this.props.prizes)
   return (
     
     <Card className="card">
@@ -27,22 +30,25 @@ function RaffleItems(props) {
     
       <Grid container justify="center" alignItems="center">
       
-      {shutter(index, props.prizes).map(prize => <Avatar class="flip-box" className="bigAvatar">
+      {this.props.prizes ? shutter(this.state.index, this.props.prizes).map(({image, name, quanity}) => 
+
+      <Avatar class="flip-box" className="bigAvatar">
         <Avatar class="flip-box-inner">
           <Avatar class="flip-box-front">
-            <img src={image}  />
+            <img class="Prizeimg" src={image}  />
           </Avatar>
          <Avatar class="flip-box-back">
            <h2>{name}</h2>
            <p>{quanity}</p>
           </Avatar>
         </Avatar>
-      </Avatar>)}
+      </Avatar>):null}
       
     </Grid>
     </CardContent>
     </Card>
   );
+}
 }
 
 
