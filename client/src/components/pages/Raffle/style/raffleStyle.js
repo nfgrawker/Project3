8d8e704c003a0ceaa364c6dfcfe18f28eb9fda43
Hertz <43124833+hertz043@users.noreply.github.com>
2018-12-04 01:15:25 -0600

    Raffle page built out with filler data/images

--- client/src/components/pages/Raffle/style/raffleStyle.js
+++ client/src/components/pages/Raffle/style/raffleStyle.js
@@ -31,14 +31,6 @@ const rafflePageStyle = {
     borderRadius: "6px",
     boxShadow:
       "0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)"
-  },
-  li: {
-    height: "0px !important",
-    boxShadow: "0px 1px 3px 0px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12)",
-    borderRadius: "4px"
-  },
-  h4: {
-    padding: [5, 5, 5, 5]
   }
 
 };
