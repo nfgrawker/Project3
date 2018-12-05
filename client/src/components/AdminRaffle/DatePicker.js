800b0254eedc23d354ca1649434510442391b8b4
kkasperek <kt.kasperek@gmail.com>
2018-12-05 17:06:45 -0600

    change to date picker

--- client/src/components/AdminRaffle/DatePicker.js
+++ client/src/components/AdminRaffle/DatePicker.js
@@ -30,38 +30,3 @@ export default class SelectDate extends PureComponent {
     );
   }
 }
-// import React, { PureComponent } from 'react';
-// import DateFnsUtils from '@date-io/moment';
-// import { MuiPickersUtilsProvider } from 'material-ui-pickers';
-// import { DateTimePicker } from 'material-ui-pickers';
-
-// export default class SelectDate extends PureComponent {
-//   state = {
-//     selectedDate: new Date()
-//   };
-
-//   handleDateChange = (event, date) => {
-//     this.setState({ selectedDate: date });
-//      console.log(this.state.selectedDate);
-//   };
-
-//   render() {
-      
-//     return (
-//       <MuiPickersUtilsProvider utils={DateFnsUtils}>
-//         <div className="pickers">
-//           <DateTimePicker
-//            keyboard
-//            autoOk
-//            clearable
-//            disablePast
-//            name={this.props.name}
-//            format="MMMM-DD-YYYY HH:mm"
-//             value={this.state.selectedDate}
-//             onChange={this.handleDateChange}
-//           />
-//         </div>
-//       </MuiPickersUtilsProvider>
-//     );
-//   }
-// }
