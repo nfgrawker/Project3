cbe9f1139cafb76df1048eb117a96e4d0b1321e1
kkasperek <kt.kasperek@gmail.com>
2018-12-05 19:51:32 -0600

    sending raffles to db

--- client/src/components/AdminRaffle/DatePicker.js
+++ client/src/components/AdminRaffle/DatePicker.js
@@ -1,4 +1,5 @@
 import React, { Fragment, PureComponent } from 'react';
+import moment from 'moment';
 import { DateTimePicker } from 'material-ui-pickers';
 
 export default class SelectDate extends PureComponent {
@@ -9,7 +10,12 @@ export default class SelectDate extends PureComponent {
   };
 
   handleDateChange = date => {
-    this.setState({ selectedDate: date });
+    let d = moment(date).format('MM-DD-YYYY HH:mm:ss');
+    console.log(d);
+    this.setState({ 
+        selectedDate: date,
+        startTime: d, 
+    });
   };
 
   render() {
@@ -19,10 +25,11 @@ export default class SelectDate extends PureComponent {
       <Fragment>
         <div className="picker">
           <DateTimePicker
+            time={this.state.startTime}
             value={selectedDate}
             disablePast
             onChange={this.handleDateChange}
-            label="With Today Button"
+            label={this.props.label}
             showTodayButton
           />
         </div>
