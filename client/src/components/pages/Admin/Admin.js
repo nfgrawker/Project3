f52e34b2f708ff67b99c1656ef59e5771550a5f0
kkasperek <kt.kasperek@gmail.com>
2018-12-06 19:11:42 -0600

    style changes

--- client/src/components/pages/Admin/Admin.js
+++ client/src/components/pages/Admin/Admin.js
@@ -23,7 +23,9 @@ const styles = theme => ({
     display: "flex"
   },
   welcome: {
-    textAlign: 'center'
+    textAlign: "center",
+    padding: "40px 10px",
+    
   },
   appBar: {
     zIndex: theme.zIndex.drawer + 1
@@ -141,7 +143,7 @@ class AdminPage extends Component {
 
         {/* Main Content */}
         <main className={classes.content}>
-          <h4 className={classes.welcome}>Welcome Back {this.state.username}</h4>
+          <h4 className={classes.welcome}>Welcome Back, {this.state.username}</h4>
           <AdminMain {...this.props}>
             {this.renderMainContent(this.props)}
           </AdminMain>
